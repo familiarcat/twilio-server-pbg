051b351b0b4ba44a34afd3d3ceb5112033c59bd7
Brady Georgen <pbradygeorgen@me.com>
2013-11-07 19:31:09 -0600

    added twilio.yml

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -12,7 +12,7 @@ class TextsController < ApplicationController
 
          @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
-         @twilio_client.account.sms.messages.create(
+        message = @twilio_client.account.sms.messages.create(
            :from => "+1#{twilio_phone_number}",
            :to => number_to_send_to,
            :body => "This is a message. It gets sent to #{number_to_send_to}"
