18f2416ef2d80b4b0a870d21e38b293db4eb47b5
Brady Georgen <pbradygeorgen@me.com>
2013-11-07 19:17:12 -0600

    updated controller as client

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,10 +4,20 @@ class TextsController < ApplicationController
   def index 
      message_body = params["Body"]
      from_number = params["From"]
-     twiml = Twilio::TwiML::Response.new do |r|
-       r.Message "Hey Monkey. Thanks for the message!"
-     end
-     twiml.text
+     number_to_send_to = from_number
+
+         twilio_sid = "ACa09a74cf7aa3479773f1f4fb0ac4a936"
+         twilio_token = "0f8ff6f9e8fb73b2c5b7b41664960cde"
+         twilio_phone_number = "3147202048"
+
+         @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+         @twilio_client.account.sms.messages.create(
+           :from => "+1#{twilio_phone_number}",
+           :to => number_to_send_to,
+           :body => "This is a message. It gets sent to #{number_to_send_to}"
+         )
+     
      
      # respond_to do |format|
      #        format.html {render :text =>"#{twiml.text}"}
