73868e91c9f056022bcf97bc1d43d4a044bff32a
Brady Georgen <pbradygeorgen@me.com>
2013-11-11 15:29:33 -0600

    changed cross domain headers in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
+  def allow_cross_domain_access
+          response.headers["Access-Control-Allow-Origin"] = "Content-Type, X-Requested-With"
+          response.headers["Access-Control-Allow-Methods"] = ""GET, PUT, POST, DELETE""
+  end
 end
