b91155c1b661949770fc572808b4ce587636a2d1
Brady Georgen <pbradygeorgen@me.com>
2013-11-13 13:20:47 -0600

    changing control headers again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,13 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
   before_filter :cors_set_access_control_headers
   
- #yeah, let's not deploy like this in production...
 
-  # def cors_set_access_control_headers
-  #     headers['Access-Control-Allow-Origin'] = '*'# need to be changed once it goes to production 'http://localhost:8080'
-  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  #     headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
-  #     headers['Access-Control-Max-Age'] = "1728000"
-  #   end 
+  def cors_set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'# need to be changed once it goes to production 'http://localhost:8080'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
+        headers['Access-Control-Max-Age'] = "1728000"
+      end 
   
   def update_message_user_names()
     @messages = Message.all
