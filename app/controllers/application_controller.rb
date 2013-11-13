35e1ef0df54a221d50b886bef662d0141ec22736
Brady Georgen <pbradygeorgen@me.com>
2013-11-13 14:37:21 -0600

    changing localhost to domain namespace in whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   
 
   def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'# need to be changed once it goes to production 'http://localhost:8080'
+        headers['Access-Control-Allow-Origin'] = 'http://twilio-server-pbg.herokuapp.com/'# need to be changed once it goes to production 'http://localhost:8080'
         headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
         headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
         headers['Access-Control-Max-Age'] = "1728000"
